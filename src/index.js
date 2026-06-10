d9a575c09bd5aa38b61d5ba8d7a1ec77b9a5013c
Shuo Wu <sw6529@nyu.edu>
2026-06-10 00:10:19 -0400

    Updated template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "./styles.css";
 import { testing } from "./export-template.js";
 
+/**
+ * npx eslint dir/file.js
+ * npx prettier . --check
+ * npx prettier . --write
+ * npm run dev
+ * npm run build
+ **/
+
 console.log(testing);
